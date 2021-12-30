f330af84d4c9580bfb67843899ffb203149eb23a
Sulfikkar E S <95119002+SulfikkarES@users.noreply.github.com>
2021-12-30 19:57:11 +0530

    Update City.cs

--- EazyLern.PersonManagementSystemSulfikkar/Components/City.cs
+++ EazyLern.PersonManagementSystemSulfikkar/Components/City.cs
@@ -1,6 +1,6 @@
 ﻿#region Copyright EazyLearn
 //
-// All rights are reserved. Reproduction or transmission in whole or in part, 74
+// All rights are reserved. Reproduction or transmission in whole or in part, 
 // in any form or by any means, electronic, mechanical or otherwise, is
 // prohibited without the prior written consent of the copyright owner.
 //
